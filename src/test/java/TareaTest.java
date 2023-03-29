8e3a42a1d57838902dd964292c0314ccfbb844f2
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-03-29 10:04:56 -0300

    subo los test

---
+++ src/test/java/TareaTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TareaTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    public void isPrime() {
+    }
+
+    @Test
+    void copyVectors() {
+    }
+}
